aed1805b5bac0bb9b82af602dfdb139fbd51b223
Sam-run <68461460+Sam-run@users.noreply.github.com>
2022-05-16 19:21:28 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,19 +6,22 @@ import (
 	"github.com/Sam-run/GoModule-GinDemo/controller"
 
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/gorm"
 )
 
 func main() {
-	// //Default返回一个默认的路由引擎
-	// r := gin.Default()
-	// r.GET("/ping", func(c *gin.Context) {
-	//
-	// 	//输出结果
-	// 	c.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
-	// r.Run()
+
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	type user struct {
+		gorm.Model
+		account  string
+		password string
+		name     string
+		mobile   uint
+	}
 	router := gin.Default()
 	//StaticFS，是加载完整的目录资源
 	router.StaticFS("/public", http.Dir("/Users/shenzhu/Downloads/Go/GinDemo/web/static"))
